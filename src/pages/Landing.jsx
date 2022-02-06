db43f419a9020535a91b46059e9cf8e24bb00ded
Marek C <you@example.com>
2022-02-06 13:51:15 -0500

    ADD: changes to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,6 +4,7 @@ import backgroundVideo from "../assets/video/Pexels Videos 2014792.mp4";
 import church from "../assets/church-svgrepo-com.svg";
 import videoCall from "../assets/video-call-svgrepo-com.svg";
 import charity from "../assets/charity-svgrepo-com.svg";
+import { Fade, Flip, Slide } from "react-reveal";
 
 function Landing() {
 	return (
@@ -14,63 +15,71 @@ function Landing() {
 				</video>
 				<div id="overlay">OVERLAY</div>
 				<section className="landingContent">
-					<h1>Embrace Christian Church</h1>
-					<h2>Let's all grow together in Christ</h2>
-					<h4>
-						Welcome to ECC where our home is your home! Click on the buttons
-						below to find out more about our great community.
-					</h4>
+					<Fade top>
+						<h1>Embrace Christian Church</h1>
+						<h2>Let's all grow together in Christ</h2>
+						<h4>
+							Welcome to ECC where our home is your home! Click on the buttons
+							below to find out more about our great community.
+						</h4>
+					</Fade>
 					<div className="landingButton">
-						<button className="btn btn--home">Join Our Home</button>
-						<button className="btn btn-giving">Giving</button>
+						<Fade left>
+							<button className="btn btn--home">Join Our Home</button>
+						</Fade>
+						<Fade right>
+							<button className="btn btn-giving">Giving</button>
+						</Fade>
 					</div>
 				</section>
 			</div>
 			<section className="landingSection">
 				<h2 className="benefitsTitle">Why join ECC?</h2>
 				<div className="benefitsCards">
-					<div className="card landing--card">
-						<img
-							className="benefitsImg filter-green"
-							src={church}
-							alt="church image"
-						/>
-						<h3>Great Community</h3>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-							numquam tenetur, quos, reiciendis iste exercitationem qui nulla
-							ipsam beatae commodi labore mollitia doloribus? Aliquid voluptatem
-							dolorum earum odio ipsa modi.
-						</p>
-					</div>
-					<div className="card landing--card">
-						<img
-							className="benefitsImg filter-blue"
-							src={videoCall}
-							alt="video call"
-						/>
-						<h3>Worship from home</h3>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-							numquam tenetur, quos, reiciendis iste exercitationem qui nulla
-							ipsam beatae commodi labore mollitia doloribus? Aliquid voluptatem
-							dolorum earum odio ipsa modi.
-						</p>
-					</div>
-					<div className="card landing--card">
-						<img
-							className="benefitsImg filter-pink"
-							src={charity}
-							alt="charity"
-						/>
-						<h3>Tithes benefit charities</h3>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-							numquam tenetur, quos, reiciendis iste exercitationem qui nulla
-							ipsam beatae commodi labore mollitia doloribus? Aliquid voluptatem
-							dolorum earum odio ipsa modi.
-						</p>
-					</div>
+					<Flip top>
+						<div className="card landing--card">
+							<img
+								className="benefitsImg filter-green"
+								src={church}
+								alt="church image"
+							/>
+							<h3>Great Community</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+								numquam tenetur, quos, reiciendis iste exercitationem qui nulla
+								ipsam beatae commodi labore mollitia doloribus? Aliquid
+								voluptatem dolorum earum odio ipsa modi.
+							</p>
+						</div>
+						<div className="card landing--card">
+							<img
+								className="benefitsImg filter-blue"
+								src={videoCall}
+								alt="video call"
+							/>
+							<h3>Worship from home</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+								numquam tenetur, quos, reiciendis iste exercitationem qui nulla
+								ipsam beatae commodi labore mollitia doloribus? Aliquid
+								voluptatem dolorum earum odio ipsa modi.
+							</p>
+						</div>
+						<div className="card landing--card">
+							<img
+								className="benefitsImg filter-pink"
+								src={charity}
+								alt="charity"
+							/>
+							<h3>Tithes benefit charities</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+								numquam tenetur, quos, reiciendis iste exercitationem qui nulla
+								ipsam beatae commodi labore mollitia doloribus? Aliquid
+								voluptatem dolorum earum odio ipsa modi.
+							</p>
+						</div>
+					</Flip>
 				</div>
 				<button className="btn btn--home">Join Our Home</button>
 			</section>
